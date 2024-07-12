6ab648c2759fdc93a2c3d6159870cc7325239866
Alaiv <akudrjavtsev@action.tech>
2024-07-12 13:04:36 +0300

    add createGetter, invertObject functions

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const splitPaths = path ? path.split('.') : [];
 
+  return (obj) => splitPaths.reduce((acc, key) => {
+    return (acc && acc.hasOwnProperty(key)) ? acc[key] : undefined;
+  }, obj);
 }
