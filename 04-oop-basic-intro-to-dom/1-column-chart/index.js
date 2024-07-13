ed17706477c514f7d21f1e9efab63f5fe25cd7a9
Alaiv <akudrjavtsev@action.tech>
2024-07-13 18:38:05 +0300

    refactor to use templates

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -15,59 +15,76 @@ export default class ColumnChart {
     this._formatHeading = formatHeading;
     this._data = this.getHeightNormalizedElements(data);
 
-    this._element = this.createNodes();
+    this._element = this.createElement();
   }
 
   get element() {
     return this._element;
   }
 
-  createNodes() {
-    const element = document.createElement('div');
-    element.classList.add('column-chart');
+  createElement() {
+    const rootElement = document.createElement("div");
+    rootElement.innerHTML = this.createChartTemplate();
 
     if (!this._data || !this._data.length) {
-      element.classList.add('column-chart_loading');
+      rootElement.firstElementChild.classList.add('column-chart_loading');
     }
 
-    element.appendChild(this.createTitle());
-    element.appendChild(this.createContainer());
-
-    return element;
+    return rootElement.firstElementChild;
   }
 
-  createTitle() {
-    const title = new ChartTitle({
-      label: this._label,
-      link: this._link,
-    });
-
-    return title.element;
+  createChartTemplate() {
+    return (
+      `<div class="column-chart">
+         <div class="column-chart__title">
+            Total ${this._label}
+            ${this.createLink('View all')}
+         </div>
+         <div class="column-chart__container">
+            ${this.createHeader()}
+            ${this.createBody()}
+         </div>
+       </div>`
+    );
   }
 
-  createContainer() {
-    this._chartContainerObject = new ChartContainer({
-      data: this._data,
-      value: this._value,
-      formatHeading: this._formatHeading
-    });
+  createLink(linkText) {
+    return this._link
+      ? `<a href=${this._link} class="column-chart__link">${linkText}</a>`
+      : '';
+  }
 
-    return this._chartContainerObject.element;
+  createHeader() {
+    return (
+      `<div data-element="header" class="column-chart__header">
+            ${this._formatHeading(this._value)}
+      </div>`
+    );
+  }
+
+  createBody() {
+    return (
+      `<div data-element="body" class="column-chart__chart">
+          ${this.getBodyItems()}
+      </div>`
+    );
+  }
+
+  getBodyItems() {
+    return this._data.map(({value, percent}) => (
+      `<div style="--value: ${value}" data-tooltip="${percent}%"></div>`
+    )).join('');
   }
 
   update(newData) {
-    if (!newData || !newData.length) {
-      this._element.classList.add('column-chart_loading');
-      return;
+    if (newData && newData.length) {
+      this._element.classList.remove('column-chart_loading');
     }
 
-    this._element.classList.remove('column-chart_loading');
-    const normalizedData = this.getHeightNormalizedElements(newData);
-    this._chartContainerObject.updateChartBody(normalizedData);
-  }
+    const chartBody = document.querySelector('.column-chart__chart');
+    this._data = this.getHeightNormalizedElements(newData);
 
-  remove() {
-    this._element.remove();
+    chartBody.innerHTML = this.getBodyItems();
   }
 
   getBaseChartHeight() {
@@ -97,106 +114,8 @@ export default class ColumnChart {
     this.remove();
     delete this;
   }
-}
 
-class ChartTitle {
-  constructor({label, link}) {
-    this._label = label;
-    this._link = link;
-
-    this._element = this.createNodes();
-  }
-
-  get element() {
-    return this._element;
-  }
-
-  createNodes() {
-    const element = document.createElement('div');
-    element.className = 'column-chart__title';
-    element.textContent = `Total ${this._label}`;
-
-    if (this._link) {
-      element.appendChild(this.createLink());
-    }
-
-    return element;
-  }
-
-  createLink() {
-    const salesLink = document.createElement('a');
-    salesLink.classList.add('column-chart__link');
-    salesLink.textContent = 'View all';
-    salesLink.href = this._link;
-
-    return salesLink;
-  }
-}
-
-class ChartContainer {
-  constructor({value, data, formatHeading}) {
-    this._value = value;
-    this._data = data;
-    this._formatHeading = formatHeading;
-
-    this._element = this.createNodes();
-  }
-
-  get element() {
-    return this._element;
-  }
-
-  createNodes() {
-    const element = document.createElement('div');
-    element.classList.add('column-chart__container');
-
-    element.appendChild(this.createHeader());
-    element.appendChild(this.createChartBody());
-
-    return element;
-  }
-
-  createHeader() {
-    const header = document.createElement('div');
-    header.classList.add('column-chart__header');
-    header.setAttribute("data-element", "header");
-    header.textContent = this._formatHeading(this._value);
-
-    return header;
-  }
-
-  createChartBody() {
-    const chartBody = document.createElement('div');
-    chartBody.classList.add('column-chart__chart');
-    chartBody.setAttribute("data-element", "body");
-
-    chartBody.append(...this.getContainerItems(this._data));
-
-    return chartBody;
-  }
-
-  updateChartBody(newData) {
-    const chartBody = document.querySelector('.column-chart__chart');
-    chartBody.innerHTML = '';
-    chartBody.append(...this.getContainerItems(newData));
-  }
-
-  getContainerItems(data) {
-    if (!data) {
-      return [];
-    }
-
-    return data.map((item) => ChartContainerItem.create(item));
-  }
-}
-
-class ChartContainerItem {
-  static create({value, percent}) {
-    const chartElement = document.createElement("div");
-
-    chartElement.style.setProperty("--value", value?.toString());
-    chartElement.setAttribute("data-tooltip", `${percent}%`);
-
-    return chartElement;
+  remove() {
+    this._element.remove();
   }
 }
