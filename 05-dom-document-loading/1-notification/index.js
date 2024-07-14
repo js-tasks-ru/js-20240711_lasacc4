157390562b7983f6186e7cac20d19778fe0f7107
Alaiv <akudrjavtsev@action.tech>
2024-07-14 15:44:26 +0300

    add NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,92 @@
 export default class NotificationMessage {
+  static _activeNotification;
+  static _timeout;
 
+  constructor(message = "", props = {}) {
+    const {
+      duration = 2000,
+      type = "success"
+    } = props;
+
+    this._message = message;
+    this._duration = duration;
+    this._type = type;
+
+    this._element = this.createElement();
+  }
+
+  get element() {
+    return this._element;
+  }
+
+  get duration() {
+    return this._duration;
+  }
+
+  createElement() {
+    const root = document.createElement('div');
+    root.innerHTML = this.createNotificationTemplate();
+
+    return root.firstElementChild;
+  }
+
+  createNotificationTemplate() {
+    return (
+      `<div
+          class="notification ${this._type}"
+          style="--value:${this.getDurationInSeconds()}s"
+       >
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+             ${this.createWrapperContent()}
+          </div>
+       </div>`
+    );
+  }
+
+  getDurationInSeconds() {
+    return this._duration / 1000;
+  }
+
+  createWrapperContent() {
+    return (
+      `<div class="notification-header">
+           ${this._type}
+       </div>
+       <div class="notification-body">
+          ${this._message}
+       </div>`
+    );
+  }
+
+  show(rootElement) {
+    if (NotificationMessage._activeNotification) {
+      this.removeActiveNotification();
+    }
+
+    const root = rootElement ?? document.body;
+    root.append(this._element);
+
+    NotificationMessage._activeNotification = this._element;
+    NotificationMessage._timeout = setTimeout(() => this.remove(), this._duration);
+  }
+
+  remove() {
+    this._element.remove();
+    NotificationMessage._activeNotification = undefined;
+  }
+
+  removeActiveNotification() {
+    if (NotificationMessage._timeout) {
+      clearTimeout(NotificationMessage._timeout);
+    }
+
+    NotificationMessage._activeNotification?.remove();
+    NotificationMessage._activeNotification = undefined;
+  }
+
+  destroy() {
+    this.remove();
+    delete this;
+  }
 }
