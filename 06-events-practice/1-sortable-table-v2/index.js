bb60c0092bd7985fee455863710b1c79fab917d9
Alaiv <akudrjavtsev@action.tech>
2024-07-18 14:45:59 +0300

    use inheritance for sortableTable component

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,9 +1,10 @@
 import {sortByFields} from "./utils.js";
+import SortableTableV1 from "../../05-dom-document-loading/2-sortable-table-v1/index.js";
 
-export default class SortableTable {
+export default class SortableTable extends SortableTableV1 {
   _validSortTypes = ["number", "string"];
 
-  constructor(headersConfig, props = {}) {
+  constructor(headerConfig, props = {}) {
     const {
       data = [],
       sorted = {
@@ -14,19 +15,14 @@ export default class SortableTable {
       customUserSortField
     } = props;
 
-    this._headersConfig = headersConfig;
-    this._data = data;
-    this._order = sorted.order;
+    super(headerConfig, data);
+
+    this._currentOrder = sorted.order;
     this._isSortLocally = isSortLocally;
     this._customUserSortField = customUserSortField;
 
     this.sort(sorted.id);
-
-    this._element = this.createContainerElement(sorted);
-  }
-
-  get element() {
-    return this._element;
+    this.subElements.header.addEventListener('pointerdown', this.onHeaderPointerDownListener.bind(this));
   }
 
   onHeaderPointerDownListener(e) {
@@ -41,18 +37,28 @@ export default class SortableTable {
 
     if (isSortable) {
       this.sort(id);
-
-      this.subElements.header.innerHTML = this.createHeaderCellsTemplate();
-      this.subElements.body.innerHTML = this.createBodyRowsTemplate();
     }
   }
 
-  sort(field) {
-    if (!this._data.length) {
+  sort(field, order) {
+    const {sortType, sortable} = this._headerConfig.find(item => item.id === field);
+
+    if (!this._data.length || !sortable) {
       return;
     }
 
-    const {sortType} = this._headersConfig.find(item => item.id === field);
+    const sortFields = this.getSortFields(field, sortType);
+    const sortOrder = order ?? this._currentOrder;
+
+    this._data = this.getSortedData(sortFields, sortOrder);
+    this._currentSortField = field;
+    this._currentOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+
+    this.subElements.header.innerHTML = this.createHeaderCellsTemplate();
+    this.subElements.body.innerHTML = this.createBodyRowsTemplate();
+  }
+
+  getSortFields(field, sortType) {
     const sortFields = [{value: field, type: sortType}];
 
     if (this._customUserSortField) {
@@ -63,126 +69,15 @@ export default class SortableTable {
       }
     }
 
-    this._data = this.getSortedData(sortFields);
-    this._currentSortField = field;
-    this._order = this._order === 'asc' ? 'desc' : 'asc';
+    return sortFields;
   }
 
-  getSortedData(fields) {
+  getSortedData(fields, order) {
     if (this._isSortLocally) {
-      return sortByFields(this._data, fields, this._order);
+      return sortByFields(this._data, fields, order);
     }
 
     // placeholder for server sorting
     return this._data;
-  }
-
-  createContainerElement() {
-    const element = document.createElement("div");
-    element.innerHTML = this.createContainerTemplate();
-
-    const containerElement = element.firstElementChild;
-    const tableElement = containerElement.firstElementChild;
-
-    if (this._data.length) {
-      tableElement.classList.remove('sortable-table_loading', 'sortable-table_empty');
-    }
-
-    this.subElements = this.getSubElements(containerElement);
-    this.subElements.header.addEventListener('pointerdown', this.onHeaderPointerDownListener.bind(this));
-    return containerElement;
-  }
-
-  getSubElements(element) {
-    const elements = element.querySelectorAll("[data-element]");
-    const result = {};
-
-    elements.forEach((subElement) => {
-      const name = subElement.dataset.element;
-      result[name] = subElement;
-    });
-
-    return result;
-  }
-
-  createContainerTemplate() {
-    return (
-      `<div data-element="productsContainer" class="products-list__container">
-            <div class="sortable-table sortable-table_loading sortable-table_empty">
-                <div data-element="header" class="sortable-table__header sortable-table__row">
-                    ${this.createHeaderCellsTemplate()}
-                </div>
-                <div data-element="body" class="sortable-table__body">
-                    ${this.createBodyRowsTemplate()}
-                </div>
-                ${this.createEmptyDataTemplate()}
-            </div>
-       </div>`
-    );
-  }
-
-  createHeaderCellsTemplate() {
-    return this._headersConfig.map(item => {
-      const {id, title, sortable} = item;
-      const isSortField = this._currentSortField === id;
-      const dataSort = this._order ? `data-order="${this._order}"` : '';
-
-      return (
-        `<div
-            class="sortable-table__cell"
-            data-id=${id}
-            data-sortable=${sortable}
-            ${dataSort}
-        >
-            <span>${title}</span>
-            ${isSortField ? this.createSortArrowTemplate() : ''}
-        </div>`
-      );
-    }).join('');
-  }
-
-  createSortArrowTemplate() {
-    return (
-      `<span data-element="arrow" class="sortable-table__sort-arrow">
-          <span class="sort-arrow"></span>
-       </span>`
-    );
-  }
-
-  createBodyRowsTemplate() {
-    return this._data
-      .map((item) => this.createRowTemplate(item, this._headersConfig))
-      .join('');
-  }
-
-  createRowTemplate(item) {
-    const cells = this._headersConfig.map(({id, template}) => {
-      return template ? template(item[id]) : `<div class="sortable-table__cell">${item[id]}</div>`;
-    }).join('');
-
-    return (
-      `<a href="/products/${item.id}" class="sortable-table__row">
-            ${cells}
-       </a>`
-    );
-  }
-
-  createEmptyDataTemplate() {
-    return (
-      `<div data-element="loading" class="loading-line sortable-table__loading-line"></div>
-       <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
-           <div>
-              <p>No products satisfies your filter criteria</p>
-              <button type="button" class="button-primary-outline">
-                 Reset all filters
-              </button>
-           </div>
-       </div>`
-    );
-  }
-
-  destroy() {
-    this.subElements.header.removeEventListener('pointerdown', this.onHeaderPointerDownListener.bind(this));
-    this._element.remove();
   }
 }
