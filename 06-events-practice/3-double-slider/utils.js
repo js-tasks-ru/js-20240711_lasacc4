606f7506f7bad0a41159d28d397d35a010f7ccfa
Alaiv <akudrjavtsev@action.tech>
2024-07-20 19:58:39 +0300

    add DoubleSlider component

---
+++ 06-events-practice/3-double-slider/utils.js
@@ -0,0 +1,12 @@
+export const borderValueValidator = {
+  left: (props) => {
+    const {value, min, selected} = props;
+    const tempValue = Math.min(value, selected.to);
+    return Math.max(tempValue, min);
+  },
+  right: (props) => {
+    const {value, max, selected} = props;
+    const tempValue = Math.max(value, selected.from);
+    return Math.min(tempValue, max);
+  },
+};
