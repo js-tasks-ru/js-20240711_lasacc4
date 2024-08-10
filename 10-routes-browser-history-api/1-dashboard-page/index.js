1235cd15255887e4b3e9b3ed3fa5bcb9b5d79e28
Alaiv <akudrjavtsev@action.tech>
2024-08-10 13:35:46 +0300

    add DashBoard Page component, fix infiniteScroll bug

--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -1,12 +1,129 @@
-import RangePicker from './components/range-picker/src/index.js';
-import SortableTable from './components/sortable-table/src/index.js';
-import ColumnChart from './components/column-chart/src/index.js';
-import header from './bestsellers-header.js';
+import RangePicker from "../../08-forms-fetch-api-part-2/2-range-picker/index.js";
+import SortableTable from "../../07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js";
+import ColumnChart from "../../07-async-code-fetch-api-part-1/1-column-chart/index.js";
+import header from "./bestsellers-header.js";
 
-import fetchJson from './utils/fetch-json.js';
+import fetchJson from "./utils/fetch-json.js";
 
-const BACKEND_URL = 'https://course-js.javascript.ru/';
+const BACKEND_URL = "https://course-js.javascript.ru";
 
 export default class Page {
+  element;
+  components;
 
+  constructor() {
+    const today = new Date();
+    const range = {
+      from: new Date(today.getFullYear(), today.getMonth() - 1),
+      to: today,
+    };
+
+    this.components = {
+      rangePicker: new RangePicker(range),
+      ordersChart: new ColumnChart({
+        url: "api/dashboard/orders",
+        label: "orders",
+        link: "#",
+        range: range,
+      }),
+      salesChart: new ColumnChart({
+        url: "api/dashboard/sales",
+        label: "sales",
+        formatHeading: (data) => `$${data}`,
+        range: range,
+      }),
+      customersChart: new ColumnChart({
+        url: "api/dashboard/customers",
+        label: "customers",
+        range: range,
+      }),
+      sortableTable: new SortableTable(header, {
+        url: "api/dashboard/bestsellers",
+        isSortLocally: true,
+        dateRange: range,
+      }),
+    };
+  }
+
+  async render() {
+    const rootElement = document.createElement("div");
+    rootElement.innerHTML = this.createTempalte();
+
+    this.element = rootElement.firstElementChild;
+    this.subElements = this.getSubElements();
+    this.initComponentElements();
+    this.createEventListeners();
+
+    return this.element;
+  }
+
+  onRangePickerDateSelect = (e) => {
+    Object.values(this.components).forEach(async (component) => {
+      const { from, to } = e.detail;
+
+      if (component instanceof ColumnChart) {
+        await component.update(from, to);
+      }
+
+      if (component instanceof SortableTable) {
+        await component.updateItems(e.detail);
+      }
+    });
+  };
+
+  createEventListeners() {
+    this.components.rangePicker.element.addEventListener(
+      "date-select",
+      this.onRangePickerDateSelect
+    );
+  }
+
+  destroyEventListeners() {
+    this.components.rangePicker.element.removeEventListener(
+      "date-select",
+      this.onRangePickerDateSelect
+    );
+  }
+
+  createTempalte() {
+    return `<div class="dashboard">
+                <div class="content__top-panel">
+                    <h2 class="page-title">Dashboard</h2>
+                    <div data-element="rangePicker"></div>
+                </div>
+                <div data-element="chartsRoot" class="dashboard__charts">
+                    <div data-element="ordersChart" class="dashboard__chart_orders"></div>
+                    <div data-element="salesChart" class="dashboard__chart_sales"></div>
+                    <div data-element="customersChart" class="dashboard__chart_customers"></div>
+                </div>
+                <h3 class="block-title">Best sellers</h3>
+                <div data-element="sortableTable"></div>
+            </div>`;
+  }
+
+  getSubElements() {
+    const elements = this.element.querySelectorAll("[data-element]");
+
+    return [...elements].reduce((acc, element) => {
+      const value = element.dataset.element;
+      acc[value] = element;
+
+      return acc;
+    }, {});
+  }
+
+  initComponentElements() {
+    Object.entries(this.components).forEach(([name, component]) => {
+      this.subElements[name].append(component.element);
+    });
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.destroyEventListeners();
+    this.remove();
+  }
 }
